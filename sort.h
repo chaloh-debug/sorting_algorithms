5aba5466c83ba30a91d662c8faf0fccc11260673
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-02-26 05:56:47 +0300

    add prototypes

--- sort.h
+++ sort.h
@@ -1,17 +1,17 @@
-#ifndef SORT
-#define SORT
+#ifndef SORT_H
+#define SORT_H
+
+
 #include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @next: Pointer to the next element of the list
  * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -20,21 +20,28 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/* Print Functions */
+
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+/* Sorting Algorithms Functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void my_sort(int *array, int low, int high, size_t size);
-int partition(int *array, int low, int high, size_t size);
-void swap(int *array, int i, int j);
-void merge_sort(int *array, size_t size);
-void sort(int *array, int low, int high, int *temp);
-void merge(int *arrayA, int low, int mid, int high, int *temp);
-int len(int *array);
-void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t size);
-void sift_down(int *array, int low, int high, size_t size);
 
-#endif /* SORT */
+
+/*Advanced Tasks*/
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
